099743441532f2ae7f7c9456d704b0dffa8c8370
Lovepreet Singh Maan <lsinghmaan@console.com.au>
2020-12-17 17:02:42 +1000

    "Lifting state not working - undefined prop"

--- src/App.js
+++ src/App.js
@@ -4,17 +4,58 @@ import "bootstrap/dist/css/bootstrap.css";
 import './App.css';
 import Counters from './components/Counters';
 
-function App() 
+
+class App extends Component() 
 {
+  state = { 
+    counters:[
+        {id: 1, value:1},
+        {id: 2, value:0},
+        {id: 3, value:0},
+        {id: 4, value:0},
+    ]
+ };
+
+ handelIncrement = counter => {
+     const counters = [...this.state.counters];
+     const index = counters.indexOf(counter);
+     counters[index] = {...counter};
+     counters[index].value++;
+     this.setState({counters});
+ ;}
+
+ handleDelete = (counterID) => {
+    const counters = this.state.counters.filter(c => c.id !== counterID);
+    this.setState({counters : counters})
+ };
+
+ handleReset = () =>{
+    const counters = this.state.counters.map(c=>{
+         c.value = 0;
+         return c;
+     });
+
+     this.setState({ counters });
+ };
+
+ render(){
     return ( 
-      //needed as mutiple root elements are being returned
+      // Fragement needed as mutiple root elements are being returned
       <React.Fragment>
         <NavBar />
-          <main className = 'container'>
-            <Counters />
+          <main className = "container">
+            <Counters 
+              counters = {this.state.counters}
+              onReset = {this.handleReset}
+              onIncrement = {this.handelIncrement}
+              onDelete = {this.handleDelete} 
+            />
           </main>
       </React.Fragment>
     );
+  }
+
 }
+
 
 export default App;
